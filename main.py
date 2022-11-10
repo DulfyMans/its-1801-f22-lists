62a7252ea8a2954fbdc6f49831a4cf042361a0cc
its1801f2211 <71606885+DulfyMans@users.noreply.github.com>
2022-11-10 15:34:48 +0000

    Access individual elements of a list and range of elements

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ for c in listOfColors:
     casper.left(90)
 
 
-# The structure ['red', 'green', 'blue', 'yellow'] is also a list
+# The structure ['red', 'green', 'blue', 'yellow'] is also a list()
 
 # Print just the first element of listOfColors to the console
-
+print(listOfColors[0])
 # Print just the second element of listOfColors to the console
-
+print(listOfColors[1])
 # Print just the third element of listOfColors to the console
-
+print(listOfColors[2])
 # Print just the fourth element of listOfColors to the console
+print(listOfColors[3])
+# Print from element 1 to element 2
+print(listOfColors[1:3])
+# Python will print from index 1 to 2; 3 is not included
